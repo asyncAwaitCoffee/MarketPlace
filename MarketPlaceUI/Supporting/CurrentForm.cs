13a244489e79a267cd41391624ad240d294f8f53
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-28 16:17:05 +0400

    MarketPlaceForm - BrowseItemFactory

---
+++ MarketPlaceUI/Supporting/CurrentForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlaceUI.Supporting
+{
+    internal enum CurrentForm
+    {
+        Main,
+        Browse,
+        MyItems,
+        Favorites,
+        History
+    }
+}
